c8af3c1c004037d7efb630414ed3cfda0ed23ccc
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-01-16 09:04:32 +0100

    clone_a_160123

--- java/C08tri_de_nombres2/src/C08tri_de_nombres2/App.java
+++ java/C08tri_de_nombres2/src/C08tri_de_nombres2/App.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class App {
 	
+	//Lire 3 nombres réels double précision : a,b et c. Les écrire dans l'ordre croissant
+	
 	/* Variables
 	 * 
 	 * a est un réel
@@ -24,7 +26,7 @@ public class App {
 	 * 			Ecrire a, "<" , b , "<" , c
 	 * 		Sinon // b <= a
 	 * 				Si a < c
-	 * 					Ecrire a , "<" , b
+	 * 					Ecrire a , "<" , c , "<" , b
 	 * 				Fin Si
 	 * 		Fin Si
 	 * Sinon // b <= a
@@ -45,7 +47,63 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Déclaration des variables
+		
+		double a;
+		double b;
+		double c;
+		
+		// Déclaration d'un scanner
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// Début du programme
+		
+		// Lecture des nomnbres
+		
+		System.out.println( " saisir un nombre a : ");
+		a = sc.nextDouble();
+		
+		System.out.println( " saisir un nombre b : ");
+		b = sc.nextDouble();
+		
+		System.out.println( " saisir un nombre c ");
+		c = sc.nextDouble();
+		
+		// Test
+		
+		if (a < b) {
+			if (b < c) {
+				System.out.println(a + " < " + b + " < " + c);
+			}
+			else { // c <= b 
+				if (a < c) {
+					System.out.println(a + " < " + c + " < " + b);
+				}
+				else { // c <= a
+					System.out.println(c + " < " + a + " < " + b);
+				}
+			}
+		}
+		else { // b <= a
+			if (a < c) {
+				System.out.println(b + " < " + a + " < " + c);
+			}
+			else { // c <= a
+				if (b < c) {
+					System.out.println(b + " < " + c + " < " + a);
+				}
+				else { // c <= b
+					System.out.println(c+ " < " + b + " < " + a);
+				}
+			}
+			
+			}
+		}
+		
+				
 
 	}
 
-}
+
