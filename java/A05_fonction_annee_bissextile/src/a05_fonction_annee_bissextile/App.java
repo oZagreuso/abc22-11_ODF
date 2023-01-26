12f70487252c57ffb13c269974a896dc63166560
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-01-26 15:32:34 +0100

    ComitPM_01_260123

--- java/A05_fonction_annee_bissextile/src/a05_fonction_annee_bissextile/App.java
+++ java/A05_fonction_annee_bissextile/src/a05_fonction_annee_bissextile/App.java
@@ -2,10 +2,11 @@ package a05_fonction_annee_bissextile;
 
 import java.util.Scanner;
 
-/* Enoncé : 
+/* Enonce : 
  * Ecrire et utiliser une fonction déterminant si une année passée en paramètre est bissextile ou non
  */
-/* Variables
+/* VARIABLE
+ * 
  * annee est un entier
  * 
  * 
@@ -13,8 +14,54 @@ import java.util.Scanner;
 
 public class App {
 
+public static boolean bissextile(int annee)
+{
+if (
+		annee % 4 != 0 ||
+		(annee % 100 == 0 &&
+		annee % 400 != 0)
+	) 
+{
+	return true;
+}
+
+
+else { 
+		
+	return false;
+	}
+}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Variable
+		
+		int annee;
+		
+		// Déclarer un Scanner
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// Début du programme
+		
+		System.out.println( " Indiquez une anne ");
+		annee = sc.nextInt();
+		
+		// Appeler la fonction:
+		
+		if(bissextile(annee))
+		{
+			System.out.println(" L'annee " + annee + " n'est pas bissextile ");
+		}
+		else
+		{
+			System.out.println(" L'annee " + annee + " est bissextile ");
+		}
+		
+		
+		
 
 	}
 
