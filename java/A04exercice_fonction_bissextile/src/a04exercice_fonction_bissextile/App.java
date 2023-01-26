01c38dfaca52ea7028dda07634230f8843063038
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-01-26 11:57:09 +0100

    CloneAM_03_260123

--- java/A04exercice_fonction_bissextile/src/a04exercice_fonction_bissextile/App.java
+++ java/A04exercice_fonction_bissextile/src/a04exercice_fonction_bissextile/App.java
@@ -3,6 +3,8 @@ package a04exercice_fonction_bissextile;
 import java.util.Scanner;
 
 public class App {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ java/A05_fonction_annee_bissextile/src/a05_fonction_annee_bissextile/App.java
@@ -0,0 +1,21 @@
+package a05_fonction_annee_bissextile;
+
+import java.util.Scanner;
+
+/* Enoncé : 
+ * Ecrire et utiliser une fonction déterminant si une année passée en paramètre est bissextile ou non
+ */
+/* Variables
+ * annee est un entier
+ * 
+ * 
+ */
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
