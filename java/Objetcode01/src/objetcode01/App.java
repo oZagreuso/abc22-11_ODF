aa24d3ed48f8ed6e970b9dfa18645587fdccd401
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-01-27 11:56:23 +0100

    cc

--- java/Objetcode01/src/objetcode01/App.java
+++ java/Objetcode01/src/objetcode01/App.java
@@ -1,10 +1,16 @@
 package objetcode01;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Declaration des Variables
+		
+		// eg: Bouteille test1 = new Bouteille() 
+		
 	}
 
 }
