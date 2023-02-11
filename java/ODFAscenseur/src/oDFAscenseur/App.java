de95d63570b74eea59e2b20d4ac87c6bb762d116
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-02-11 19:23:16 +0100

    CloneWE_02_110223

---
+++ java/ODFAscenseur/src/oDFAscenseur/App.java
@@ -0,0 +1,5 @@
+package oDFAscenseur;
+
+public class App {
+
+}
--- java/ODFAscenseur/src/oDFAscenseur/Ascenseur.java
+++ java/ODFAscenseur/src/oDFAscenseur/Ascenseur.java
@@ -11,6 +11,7 @@ public class Ascenseur {
 	// Déclaration des attributs
 	
 	private String marque;
+	private Boolean estOuverte;
 	private Boolean bloquerPorte;
 	private final int nombreEtages;
 	private int numeroEtage;
@@ -19,12 +20,42 @@ public class Ascenseur {
 	
 	public Ascenseur() {
 		this.marque = " ";
-		this.bloquerPorte = false;
+		this.estOuverte = false;
+		this.bloquerPorte = false;		
 		this.nombreEtages = 4;
 		this.numeroEtage = 2;
-		
-		
+	}
+	
+	// Constructeur classique
+	
+	public Ascenseur( String _marque, boolean _estOuverte, boolean _bloquerPorte, final int _nombreEtages, int _numeroEtage) {
+		this.marque = _marque;
+		this.estOuverte = _estOuverte;
+		this.bloquerPorte = _bloquerPorte;
+		this.nombreEtages = _nombreEtages;
+		this.numeroEtage = _numeroEtage;
+	}
+	
+	// Les fonctions
+	
+	
+	
+	// Postion porte ouverte ou fermée
+	
+	public boolean ouvrir() {
+	if ( bloquerPorte == false && estOuverte == false) {
+		this.estOuverte = true;
+		return true;
+	}
+	else {
+		this.estOuverte = false;
+		return false;
+	}
 	}
 	
 	
-}
+	
+	}
+	
+	
+
