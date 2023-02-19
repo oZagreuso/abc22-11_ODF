ca2443c658ab4d05f6c5dacc846c9d9d93a479da
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-02-19 21:30:02 +0100

    Clone_WE_02_190223

--- BureauReglable/src/bureauReglable/App.java
+++ BureauReglable/src/bureauReglable/App.java
@@ -3,7 +3,21 @@ package bureauReglable;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+BureauReglable lambda = new BureauReglable(); // Déclarer un bureau lambda
+		
+BureauReglable adeline = new BureauReglable(" Adeline ", false, false, 75, 150, 50); // Construire un objet bureau du nom de Adeline
+
+		String model = adeline.BureauReglable();
+		System.out.println(adeline.toString());
+		
+		boolean allumer1 = adeline.brancher();
+		System.out.println(adeline.toString());
+		
+		
+		
+		
 
 	}
 
--- BureauReglable/src/bureauReglable/BureauReglable.java
+++ BureauReglable/src/bureauReglable/BureauReglable.java
@@ -11,7 +11,9 @@ public class BureauReglable {
 	//Attributs
 	
 	private string modelBureau; 
+	private Boolean powerOn;
 	private Boolean boutonMonter;
+	private int positionBureauEnCm;
 	private final int hauteurMaxEnCm;
 	private final int hauteurMinEnCm; 
 
@@ -21,16 +23,57 @@ public class BureauReglable {
 
 	public BureauReglable() {
 			modelBureau = " ";
+			powerOn = false;
 			boutonMonter = false;
+			positionBureauEnCm = 75;
 			hauteurMaxEnCm = 150;
 			boutonMinEnCm = 50;
 			
 	// Constructeur classique
 
-	public BureauReglable(String _modelBureau, boolean _boutonMonter, final int hauteurMaxEnCm, final int hauteurMinEnCm) {
-				this.modelBureau = _modelBureau;
-				this.boutonMonter = _boutonMonter;
-				this.hauteurMaxEnCm = _hauteurMaxEnCm;
-				this.hauteurMinEnCm = _heuteurMinEnCm;
+	public BureauReglable(String _modelBureau, boolean _powerOn, boolean _boutonMonter, int positionBureauEnCm, final int hauteurMaxEnCm, final int hauteurMinEnCm) {
+			this.modelBureau = _modelBureau;
+			this.powerOn = _powerOn;
+			this.boutonMonter = _boutonMonter;
+			this.positionBureauEnCm = _positionBureauEnCm;
+			this.hauteurMaxEnCm = _hauteurMaxEnCm;
+			this.hauteurMinEnCm = _heuteurMinEnCm;
 			
+	// Nommer le bureau
+
+	public String BureauReglable() {
+			return this.modelBureau; 
+
+	
+	// Les méthodes
+			
+			// brancher le bureau
+			
+		public boolean brancher() {
+		
+			if (powerOn == false) {
+				powerOn = true; // On peut brancher le bureau
+				System.out.printl(" le robot peut être connecté au secteur ");
+				return true;
+			}
+			else {
+				// Le bureau est déjà branché
+				System.out.println(" le robot est déjà sous tension ");
+				return false;
+			}
+			
+			// débrancher le bureau
+			
+		public boolean débrancher le bureau() {
+			
+			if (powerOn == true) {
+				powerOn = false; 
+				return false;
+			}
+	}
+	
+			}
+			
+		}
+	}	
 }
